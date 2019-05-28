c2c7234a29f1ac8224a24e63a8a7efc74ef181cf
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2019-05-28 19:23:46 +0300

    feat(core): add util for creating empty db connection

--- src/examples/core.clj
+++ src/examples/core.clj
@@ -1,5 +1,13 @@
 (ns examples.core
-  (:gen-class))
+  (:gen-class)
+  (:require [datomic.api :as d]))
+
+(defn empty-db-conn []
+  "Creates empty in-memory db and returns the connection"
+  (let [db-uri "datomic:mem://vilnius-clojure"]
+    (d/delete-database db-uri)
+    (d/create-database db-uri)
+    (d/connect db-uri)))
 
 (defn -main
   "I don't do a whole lot ... yet."
